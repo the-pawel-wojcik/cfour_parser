ff6ab231f291d752b7f13808cd761f3ee2a3be51
Paweł Wójcik <pawel.wojcik@usc.com>
2023-12-12 17:31:56 -0800

    programs: Unify the programs container shape

--- programs.py
+++ programs.py
@@ -31,12 +31,18 @@ def get_stack_of_program_limits(lines):
             node = {
                 'type': 'head',
                 'line': ln,
+                'data': {
+                    'ok': True,
+                },
             }
             stack.append(node)
         elif line.strip().startswith(sec_end):
             node = {
                 'type': 'end',
                 'line': ln,
+                'data': {
+                    'ok': True,
+                },
             }
             stack.append(node)
 
@@ -58,7 +64,9 @@ def add_names_of_program_starts(stack, lines):
         # if the header is the last line of the file no name of the file
         # can be parsed
         if len(lines) == head_line + 1:
-            continue
+            print(f"Unexpected end of output at line {head_line}",
+                  file=sys.stderr)
+            limit['data']['ok'] = False
 
         name_line = lines[head_line + 1]
         program = os.path.basename(name_line.strip())
@@ -80,8 +88,8 @@ def parse_executable_finish_line(line):
     match = pattern.match(line)
     if match is None:
         print("Warning! Parsing of the '--executable <name> finished ...' line"
-              " failed.")
-        return {}
+              " failed.", file=sys.stderr)
+        return None
 
     score = {
         'name': match.group(1),
@@ -101,25 +109,42 @@ def add_names_of_program_ends(stack, lines):
 
         end_line = limit['line']
         finish_line_data = parse_executable_finish_line(lines[end_line])
-        limit.update(finish_line_data)
+
+        if finish_line_data is None:
+            limit['data']['ok'] = False
+            continue
+
+        limit['name'] = finish_line_data['name']
+        del finish_line_data['name']
+        limit['data'].update(finish_line_data)
 
     return stack
 
 
-def get_start_ends(lines):
+def find_programs(cfour):
     """
-    Returns a list of dictionaries with two eneries:
-        'start', and 'end'
-    These numbers are line numbers where a program starts or ends.
-    The first line has the number 0.
+    Input is an open file with CFOUR's output.
+    Output is a list of collected programs.
+    Each program is represented by a dictionary with:
+        'name': program name.
+        'start': line number (first line of output is numbered 1)
+        'end': line number (first line of output is numbered 1)
+        'lines': a list, each entry is one line of the program's output.
+        'sections': list()
+        'data': {
+            'ok': bool,  # True if didn't detect parsing nor program errors
+            'exit status': int(),
+            'walltime, sec': Reported execution time
+        }
     """
 
+    lines = cfour.readlines()
     stack = get_stack_of_program_limits(lines)
     add_names_of_program_starts(stack, lines)
     add_names_of_program_ends(stack, lines)
 
     # Match program starts with program ends
-    programs_limits = []
+    programs = list()
     active = []
     while len(stack) != 0:
         node = stack.pop()
@@ -139,70 +164,65 @@ def get_start_ends(lines):
         # TODO: the document might be incorrectly formatted -- but the compiler
         # should still finish its job
         if node['type'] != 'head' or match['type'] != 'end':
-            print("Error in parsing starts and ends of program sections.")
-            print("A missmatch between started and completed programs.")
-            print("The resulted partition is not reliable.")
+            print("Error in parsing starts and ends of program sections.",
+                  file=sys.stderr)
+            print("A missmatch between started and completed programs.",
+                  file=sys.stderr)
+            print("The resulted partition is not reliable.",
+                  file=sys.stderr)
             looks_good = False
 
         # Check that the program names are also alright
         if node['name'] != match['name']:
-            print("Error in parsing starts and ends of executables.")
+            print("Error in parsing starts and ends of executables.",
+                  file=sys.stderr)
             print(f"The opening of executable {node['name']} is matched with "
-                  f"{match['name']}.")
-            print("The resulted partition is not reliable.")
+                  f"{match['name']}.",
+                  file=sys.stderr)
+            print("The resulted partition is not reliable.",
+                  file=sys.stderr)
             looks_good = False
 
-        program_limits = {
-            'start': node['line'],
-            'end': match['line'],
-            'all good': looks_good,
-        }
-        programs_limits += [program_limits]
+        if node['data']['ok'] is False or match['data']['ok'] is False:
+            looks_good = False
 
-    # TODO: parsing of a program that has finished only parts of the jobs 
+        start_line = node['line']
+        end_line = match['line']
+        program = {
+            # Editors list the first line as number 1 not number 0.
+            'start': start_line + 1,
+            'end': end_line + 1,
+            'lines': lines[start_line:end_line+1]
+        }
+        if 'name' in match:
+            program['name'] = match['name']
+        program_data = node['data']
+        program_data.update(match['data'])
+        program['data'] = program_data
+        if looks_good is False:
+            program['data']['ok'] = False
+        program['sections'] = list()
+
+        programs += [program]
+
+    # TODO: parsing of a program that has finished only parts of the jobs
     # should be allowed
     if len(active) != 0:
         print("Error in parsing starts and ends of program sections.",
               file=sys.stderr)
         print("Best guess: some programs did not finish.", file=sys.stderr)
 
+    bad = [program for program in programs if program['data']['ok'] is False]
+    if len(bad) > 0:
+        print("Warning! Programs with errors detected in xcfour.",
+              file=sys.stderr)
+
+    programs = [program for program in programs if
+                program['data']['ok'] is True]
+
     # List programs in chronological execution order
-    programs_limits = programs_limits[::-1]
-    return programs_limits
-
-
-def find_programs(cfour):
-    """
-    Input is an open file with CFOUR's output.
-    Output is a list of collected programs.
-    Each program is represented by a dictionary with:
-        'name': program name.
-        'exit status':
-        'walltime, sec': Reported execution time
-        'start': line number (first line of output is numbered 1)
-        'end': line number (first line of output is numbered 1)
-        'lines': a list, each entry is one line of the program's output.
-    """
-    lines = cfour.readlines()
-    programs_start_ends = get_start_ends(lines)
-
-    for program in programs_start_ends:
-        start = program['start']
-        end = program['end']
-        program_info = parse_executable_finish_line(lines[end])
-        if program_info is None:
-            program['name'] = '???'
-            program['exit status'] = '???'
-            program['walltime, sec'] = '???'
-        else:
-            program.update(program_info)
-
-        program['lines'] = lines[start:end+1]
-        # Editors usually list the first line as number 1 not number 0.
-        program['start'] += 1
-        program['end'] += 1
-
-    return programs_start_ends
+    programs = programs[::-1]
+    return programs
 
 
 def main():
@@ -215,10 +235,10 @@ def main():
             start = program['start']
             end = program['end']
             name = program['name']
-            ext = program['exit status']
-            time = program['walltime, sec']
+            ext = program['data']['exit status']
+            time = program['data']['walltime, sec']
             msg = f"{start:5d} -- {end:5d}: {name:12s} took"
-            msg += f"{time}".rjust(10) + " sec"
+            msg += f"{time:.2f}".rjust(10) + " sec"
             if ext != 0:
                 msg += f" and returned {ext}."
             else:
