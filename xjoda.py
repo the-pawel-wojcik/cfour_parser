4681c0b5a2f66b31dbfc1e5b2e221c1067c5887e
Paweł Wójcik <pawel.wojcik5@gmail.com>
2024-04-23 11:55:55 -0700

    xjoda: Add parser of normal coordinate gradient

--- xjoda.py
+++ xjoda.py
@@ -35,6 +35,10 @@ def cool_lines_in_xjoda(xjoda):
          'name': 'qcomp',
          'type': 'start',
          },
+        {'pattern': re.compile(r'\s*Normal Coordinate Gradient'),
+         'name': 'normal coordinate gradient',
+         'type': 'start',
+         },
         # {'pattern': re.compile(r''),
         #  'name': '',
         #  'type': '',
@@ -102,6 +106,27 @@ def turn_xjoda_catches_into_sections(catches, xjoda):
 
             xjoda['sections'] += [qcomp]
 
+        elif catch['name'] == 'normal coordinate gradient':
+            name = catch['name']
+            THE_LINE = '-' * 71
+            start = catch['line']
+            if lines[start+1] != lines[start+4] != THE_LINE:
+                print(f"Error in parsing the {name} section of xjoda.",
+                      file=sys.stderr)
+
+            end = skip_to(THE_LINE, lines, start+5)
+
+            gradient = {
+                'name': 'normal coordinate gradient',
+                'start': xjoda['start'] + start,
+                'end': xjoda['start'] + end,
+                'lines': lines[start: end+1],
+                'sections': list(),
+                'data': dict(),
+            }
+
+            xjoda['sections'] += [gradient]
+
 
 def parse_xjoda_sections(xjoda):
     for section in xjoda['sections']:
@@ -143,6 +168,30 @@ def parse_xjoda_sections(xjoda):
             else:
                 section['data'] = data
 
+        if section['name'] == 'normal coordinate gradient':
+            lines = section['lines'][5:-1]
+            data = {
+                'Normal Coordinate Gradient': list(),
+            }
+            for line in lines:
+                split_line = line.split()
+                mode_no = int(split_line[0])
+                frequency = float(split_line[1])
+                grad_au = float(split_line[2])
+                grad_cm = float(split_line[3])
+                grad_eV = float(split_line[4])
+                ratio = float(split_line[4])
+                data['Normal Coordinate Gradient'] += [{
+                    'mode #': mode_no,
+                    'omega': frequency,
+                    'dE/dQ, a.u.': grad_au,
+                    'dE/dQ, cm-1': grad_cm,
+                    'dE/dQ, eV': grad_eV,
+                    'dE/dQ/omega, relative': ratio,
+                }]
+
+            section['data'].update(data)
+
 
 def parse_xjoda_program(xjoda):
 
@@ -150,12 +199,6 @@ def parse_xjoda_program(xjoda):
         xjoda['sections'] = []
 
     catches = cool_lines_in_xjoda(xjoda)
-
-    # TODO: catches should be turned into sections. Each section should
-    # contain the keys: name, start, end, lines, sections, data.
-    # The sections should be looped over and parsed if a parser is
-    # available
-
     turn_xjoda_catches_into_sections(catches, xjoda)
     parse_xjoda_sections(xjoda)
 
